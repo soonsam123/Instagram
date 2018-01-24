ed7d22343231a54a5bb14883c12a2f29f07bdf83
Soon Sam Ribeiro Santos <karatesoon@gmail.com>
2018-01-24 11:47:34 -0200

    Updating

--- app/src/main/java/com/example/karat/instagram/Profile/ProfileActivity.java
+++ app/src/main/java/com/example/karat/instagram/Profile/ProfileActivity.java
@@ -3,11 +3,14 @@ package com.example.karat.instagram.Profile;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.karat.instagram.BottomNavigationHelper;
