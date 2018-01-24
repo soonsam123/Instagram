1e0a53b6c1148e1f3d24e7d7ce017c81c8a3053f
Soon Sam Ribeiro Santos <karatesoon@gmail.com>
2018-01-24 20:25:41 -0200

    Updating

--- app/src/main/java/com/example/karat/instagram/Profile/EditProfileFragment.java
+++ app/src/main/java/com/example/karat/instagram/Profile/EditProfileFragment.java
@@ -3,6 +3,7 @@ package com.example.karat.instagram.Profile;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,4 +21,7 @@ public class EditProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_editprofile, container, false);
     }
+
+
+
 }
