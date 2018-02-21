96cf9deac1b60debea84f3e64a2bf10568cdeab7
Soon Sam Ribeiro Santos <karatesoon@gmail.com>
2018-02-21 09:52:19 -0300

    Updating

--- app/src/main/java/com/example/karat/instagram/Share/ShareActivity.java
+++ app/src/main/java/com/example/karat/instagram/Share/ShareActivity.java
@@ -5,7 +5,9 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -14,6 +16,7 @@ import android.view.MenuItem;
 import com.example.karat.instagram.Utils.BottomNavigationHelper;
 import com.example.karat.instagram.R;
 import com.example.karat.instagram.Utils.Permissions;
+import com.example.karat.instagram.Utils.SectionsPageAdapter;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 /**
@@ -29,6 +32,8 @@ public class ShareActivity extends AppCompatActivity {
     private static final int ACTIVITY_NUM = 2;
     private static final int VERIFY_PERMISSIONS_REQUEST = 1;
 
+    private ViewPager mViewPager;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,13 +47,31 @@ public class ShareActivity extends AppCompatActivity {
         }
 
 
-        // setUpBottomNavViewEx();
+    }
+
+
+    private void setUpViewPager(){
+
+        mViewPager = findViewById(R.id.viewPagerContainer);
+        TabLayout bottomTabLayout = findViewById(R.id.bottomTabLayout_activityShare);
+
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+
+        /*adapter.addFragment(new GalleryFragment());
+        adapter.addFragment(new PhotoFragment());
+
+        mViewPager.setAdapter(adapter);
+        bottomTabLayout.setupWithViewPager(mViewPager);
+
+        bottomTabLayout.getTabAt(0).setText(R.string.gallery);
+        bottomTabLayout.getTabAt(1).setText(R.string.photo);*/
 
     }
 
     public void verifyPermissions(String[] permissions){
         Log.i(TAG, "verifyPermissions: Verifying the permissions");
 
+        // Request the user's permission.
         ActivityCompat.requestPermissions(
                 ShareActivity.this,
                 permissions,
@@ -67,7 +90,7 @@ public class ShareActivity extends AppCompatActivity {
             }
         }
 
-        return false;
+        return true;
 
     }
 
@@ -85,17 +108,4 @@ public class ShareActivity extends AppCompatActivity {
         }
 
     }
-
-    /*private void setUpBottomNavViewEx(){
-
-        Log.d(TAG, "setUpBottomNavViewEx: Setting up the BottomNavViewEx for ShareActivity");
-        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavView);
-        BottomNavigationHelper.setUpBottomNavigationView(bottomNavigationViewEx);
-        BottomNavigationHelper.enablePagination(mContext, bottomNavigationViewEx);
-
-        Menu menu = bottomNavigationViewEx.getMenu();
-        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
-        menuItem.setChecked(true);
-
-    }*/
 }
